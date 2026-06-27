767fd18787b62ece9d20700630b71a45928070ff
awen-li <awen.li@usu.edu>
2026-06-26 19:51:45 -0600

    update

--- setenv.sh
+++ setenv.sh
@@ -2,10 +2,12 @@
 
 PYCLENS_TOOLS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PYCLENS_TOOLS_ROOT
+PYCLENS_ROOT="$(cd "$PYCLENS_TOOLS_ROOT/.." && pwd)"
 
 PYCLENS_HONGGFUZZ_HOME="$(cd "$PYCLENS_TOOLS_ROOT/honggfuzz" && pwd)"
 export PYCLENS_HONGGFUZZ_HOME
 
+export PATH="$PYCLENS_ROOT/data/rq2/envs/global-pylingual/bin:$PATH"
 export PATH="$PYCLENS_HONGGFUZZ_HOME:$PYCLENS_HONGGFUZZ_HOME/hfuzz_cc:$PATH"
 
 echo "PYCLENS_TOOLS_ROOT=$PYCLENS_TOOLS_ROOT"
