86fbcc58f4390f9a6a61b2d47e5efb71992f2f89
awen-li <awen.li@usu.edu>
2026-06-26 19:54:38 -0600

    update

--- setenv.sh
+++ setenv.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-PYCLENS_TOOLS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export PYCLENS_TOOLS_ROOT
-PYCLENS_ROOT="$(cd "$PYCLENS_TOOLS_ROOT/.." && pwd)"
+PYCLENS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export PYCLENS_ROOT
 
-PYCLENS_HONGGFUZZ_HOME="$(cd "$PYCLENS_TOOLS_ROOT/honggfuzz" && pwd)"
+PYCLENS_PYLINGUAL_HOME="$PYCLENS_ROOT/data/rq2/envs/global-pylingual"
+export PYCLENS_PYLINGUAL_HOME
+
+PYCLENS_HONGGFUZZ_HOME="$PYCLENS_ROOT/honggfuzz"
 export PYCLENS_HONGGFUZZ_HOME
 
-export PATH="$PYCLENS_ROOT/data/rq2/envs/global-pylingual/bin:$PATH"
-export PATH="$PYCLENS_HONGGFUZZ_HOME:$PYCLENS_HONGGFUZZ_HOME/hfuzz_cc:$PATH"
+export PATH="$PYCLENS_PYLINGUAL_HOME/bin:$PYCLENS_HONGGFUZZ_HOME:$PYCLENS_HONGGFUZZ_HOME/hfuzz_cc:$PATH"
 
-echo "PYCLENS_TOOLS_ROOT=$PYCLENS_TOOLS_ROOT"
+echo "PYCLENS_ROOT=$PYCLENS_ROOT"
+echo "PYCLENS_PYLINGUAL_HOME=$PYCLENS_PYLINGUAL_HOME"
 echo "PYCLENS_HONGGFUZZ_HOME=$PYCLENS_HONGGFUZZ_HOME"
+echo "pylingual=$(command -v pylingual || true)"
 echo "honggfuzz=$(command -v honggfuzz || true)"
